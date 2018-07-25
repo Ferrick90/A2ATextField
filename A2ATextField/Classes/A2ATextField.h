f252cfd7952e05fbbe965165e28e13586dea3ae9
Ferrick Chan <ferrick1990@hotmail.com>
2018-07-25 15:01:24 +0800

    first commit

---
+++ A2ATextField/Classes/A2ATextField.h
@@ -0,0 +1,12 @@
+//
+//  A2ATextField.h
+//  A2ATextField
+//
+//  Created by A2AGlobalNetwork on 23/07/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface A2ATextField : UIView
+
+@end
---
+++ A2ATextField/Classes/A2ATextField.m
@@ -0,0 +1,20 @@
+//
+//  A2ATextField.m
+//  A2ATextField
+//
+//  Created by A2AGlobalNetwork on 23/07/2018.
+//
+
+#import "A2ATextField.h"
+
+@implementation A2ATextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
